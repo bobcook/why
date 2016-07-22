c8307ded024195f1b0cc00d5493bf4ea2745e7ba
Bob Cook <bobcook@sunrem.com>
2016-07-22 09:17:41 -0600

    updates with a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722121806) do
+ActiveRecord::Schema.define(version: 20160722114332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,9 @@ ActiveRecord::Schema.define(version: 20160722121806) do
     t.integer  "video_file_size"
     t.datetime "video_updated_at"
     t.integer  "user_id"
+    t.integer  "ask_why_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.integer  "ask_why_id"
   end
 
   create_table "profiles", force: :cascade do |t|
