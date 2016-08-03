1a64e8e0244239b49abd88f5767dc14ee43d247c
shiv <shivpatel@gmail.com>
2016-08-03 15:36:27 +0530

    on single_page sign_up/in forget_password

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,57 @@
 var $input = $('<div class="modal-body"><input type="text" class="form-control" placeholder="Message"></div>')
+$(function() {
+  var $form_wrapper = $('#form_wrapper'),
+    //the current form is the one with class active
+    $currentForm  = $form_wrapper.children('form.active'),
+    //the change form links
+    $linkform   = $form_wrapper.find('.linkform');
+      
+  //get width and height of each form and store them for later            
+  $form_wrapper.children('form').each(function(i){
+    var $theForm  = $(this);
+    //solve the inline display none problem when using fadeIn fadeOut
+    if(!$theForm.hasClass('active'))
+      $theForm.hide();
+    $theForm.data({
+      width : $theForm.width(),
+      height  : $theForm.height()
+    });
+  });
+     //set width and height of wrapper (same of current form)
+  setWrapperWidth();
+  $linkform.bind('click',function(e){
+    var $link = $(this);
+    var target  = $link.attr('rel');
+    $currentForm.fadeOut(400,function(){
+      //remove class active from current form
+      $currentForm.removeClass('active');
+      //new current form
+      $currentForm= $form_wrapper.children('form.'+target);
+      //animate the wrapper
+      $form_wrapper.stop()
+             .animate({
+              width : $currentForm.data('width') + 'px',
+              height  : $currentForm.data('height') + 'px'
+             },500,function(){
+              //new form gets class active
+              $currentForm.addClass('active');
+              $currentForm.find('div').find('span').text("")
+              //show the new form
+              $currentForm.fadeIn(400);
+             });
+    });
+
+    e.preventDefault();
+  });
+    
+  function setWrapperWidth(){
+      $form_wrapper.css({
+        width : $currentForm.data('width') + 'px',
+        height  : $currentForm.data('height') + 'px',
+        'margin-left' : '40%'
+      });
+    }
+  });
 
 $(document).on('click', '.js-msgGroup', function () {
   $('.js-msgGroup, .js-newMsg').addClass('hide')
